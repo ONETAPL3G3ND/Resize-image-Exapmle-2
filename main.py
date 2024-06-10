c73c3ee71c20f6905cfb02e7bff7609db3d84de1
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-06-10 22:13:46 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import cv2 
+src = cv2.imread('D:/cv2-resize-image-original.png', cv2.IMREAD_UNCHANGED) 
+# set a new width in pixels 
+new_width = 300 
+# dsize 
+dsize = (new_width, src.shape[0]) 
+# resize image 
+output = cv2.resize(src, dsize, interpolation = cv2.INTER_AREA) 
+cv2.imwrite('D:/cv2-resize-image-width.png',output)
